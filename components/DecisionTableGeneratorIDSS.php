c659907779abd03c3c3872b80f05e7b86027d7e8
LedZeppe1in <tualatin32@mail.ru>
2024-02-26 11:24:26 +0800

    Fixed DecisionTableGeneratorIDSS class

--- components/DecisionTableGeneratorIDSS.php
+++ components/DecisionTableGeneratorIDSS.php
@@ -1,12 +1,9 @@
 <?php
 
-
 namespace app\components;
-
 
 use app\modules\stde\models\State;
 use app\modules\stde\models\StateProperty;
-use app\modules\stde\models\Transition;
 use app\modules\stde\models\TransitionProperty;
 
 
@@ -25,9 +22,9 @@ class DecisionTableGeneratorIDSS
         }
 
         $array_transitions = [];
-        $array_current_transition = [];
-        foreach ($states as $state)
+        foreach ($states as $state) {
             foreach ($state->transitions as $transition) {
+                $array_current_transition = [];
                 $array_current_transition['state-from'] = $transition->state_from;
                 $transition_properties = TransitionProperty::find()->where(['transition' => $transition->id])->all();
                 foreach ($transition_properties as $transition_property)
@@ -35,16 +32,11 @@ class DecisionTableGeneratorIDSS
                 $array_current_transition['state-to'] = $transition->state_to;
                 $array_transitions[] = $array_current_transition;
             }
-
+        }
         $array_transitions = array_unique($array_transitions, SORT_REGULAR);
 
-        // precossing transitions
-        //create header list
-        $array_headers = array();
-        //create array of rows
-        $array_rows = array();
-        $row = array();
-
+        $array_headers = [];
+        $array_rows = [];
         foreach ($array_transitions as $k => $v) {
             $row = [];
 
@@ -53,16 +45,17 @@ class DecisionTableGeneratorIDSS
                     $state_from = $v0;
                     //process state-from
                     $prefix = 'State::';
-                    if (isset($lookup_table[$state_from]))
-                        foreach ($array_states[$state_from] as $k1 => $v1) {
-                            if (in_array($prefix . $k1, $array_headers) == false) array_push($array_headers, $prefix . $k1); //add new header
-                            $row[$prefix . $k1] = $v1; //add new cell to row
-                        }
+                    foreach ($array_states[$state_from] as $k1 => $v1) {
+                        if (in_array($prefix . $k1, $array_headers) == false)
+                            array_push($array_headers, $prefix . $k1); //add new header
+                        $row[$prefix . $k1] = $v1; //add new cell to row
+                    }
                 }
 
                 if (($k0 != 'state-from') and ($k0 != 'state-to')) {
                     $prefix = 'State::';
-                    if (in_array($prefix . $k0, $array_headers) == false) array_push($array_headers, $prefix . $k0); //add new header
+                    if (in_array($prefix . $k0, $array_headers) == false)
+                        array_push($array_headers, $prefix . $k0); //add new header
                     $row[$prefix . $k0] = $v0; //add new cell to row
                 }
 
@@ -70,31 +63,26 @@ class DecisionTableGeneratorIDSS
                     $state_to = $v0;
                     //process state-from
                     $prefix = '#State::';
-                    if (isset($lookup_table[$state_to]))
-                        foreach ($array_states[$state_to] as $k1 => $v1) {
-                            if (in_array($prefix . $k1, $array_headers) == false) array_push($array_headers, $prefix . $k1); //add new header
-                            $row[$prefix . $k1] = $v1; //add new cell to row
-                        }
+                    foreach ($array_states[$state_to] as $k1 => $v1) {
+                        if (in_array($prefix . $k1, $array_headers) == false)
+                            array_push($array_headers, $prefix . $k1); //add new header
+                        $row[$prefix . $k1] = $v1; //add new cell to row
+                    }
                 }
             }
             array_push($array_rows, $row);
         }
 
-        //перебор заголовков и формирования строк для csv
+        // Перебор заголовков и формирования строк для csv
         $towrite_headers = '';
-        foreach ($array_headers as $k0 => $v0) {
+        foreach ($array_headers as $k0 => $v0)
             $towrite_headers .= $v0 . ';';
-        }
         $towrite_rows = '';
-
-        // rsort($array_headers);
 
         foreach ($array_rows as $k => $v) {
             $towrite_row = '';
-            foreach ($array_headers as $k0 => $v0) {
-                if (isset($v[$v0]))
-                    $towrite_row .= $v[$v0] . ';';
-            }
+            foreach ($array_headers as $k0 => $v0)
+                $towrite_row .= $v[$v0] . ';';
             $towrite_rows .= $towrite_row . "\n";
         }
 
